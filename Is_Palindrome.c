682484a55da589747f5c35fb19d4543440d8707d
Your Eshrak20 <uemran2018@gmail.com>
2023-10-31 09:23:31 +0600

    send the palindrome

--- Is_Palindrome.c
+++ Is_Palindrome.c
@@ -1,16 +1,53 @@
 #include <stdio.h>
-is_palindrome()
+#include <string.h>
+char is_palindrome(char *a, int sz)
 {
+    int i = 0;
+    int j = sz - 1;
+    int flag = 1;
+    while (i < j)
+    {
+        if (a[i] == a[j])
+        {
+            i++;
+            j--;
+        }
+        else
+        {
+            flag = 0;
+            break;
+        }
+    }
+    if (flag == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
 }
 int main()
 {
+    char a[1001];
+    scanf("%s", a);
+    int sz = strlen(a);
+    char res = is_palindrome(a, sz);
+    if (res == 0)
+    {
+        printf("x");
+    }
+    else
+    {
+        printf("Palindrome");
+    }
 
     return 0;
 }
 
 // #include <stdio.h>
 // #include <string.h>
- 
+
 // int main()
 // {
 //     char a[100005];
@@ -34,12 +71,12 @@ int main()
 //         }
 //     }
 //     if (ans == 0)
- 
+
 //     {
 //         printf("NO");
 //     }
 //     else
- 
+
 //     {
 //         printf("YES");
 //     }
